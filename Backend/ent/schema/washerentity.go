1fe43c56bd7215ff56519fe37971e8828875d9f9
Toneone11 <58927899+Toneone11@users.noreply.github.com>
2020-12-29 18:55:25 +0700

    สร้าง WasherEntity สำหรับเป็น output หลัก - close #5

---
+++ Backend/ent/schema/washerentity.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Washerentity holds the schema definition for the Washerentity entity.
+type Washerentity struct {
+	ent.Schema
+}
+
+// Fields of the Washerentity.
+func (Washerentity) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Washerentity.
+func (Washerentity) Edges() []ent.Edge {
+	return nil
+}
